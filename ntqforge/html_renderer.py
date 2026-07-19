9bee487c95ff38ff8a3d2b9be28231c321b0cd52
JensOtte83 <jens_otte@gmx.net>
2026-07-19 18:58:54 +0200

    html_renderer.py erstellen

---
+++ ntqforge/html_renderer.py
@@ -0,0 +1,18 @@
+from .renderer import Renderer
+
+
+class HTMLRenderer(Renderer):
+
+    def render(self, document):
+
+        html = "<html>\n"
+
+        html += f"<h1>{document.title}</h1>\n"
+
+        for component in document.components:
+
+            html += component.render(self)
+
+        html += "</html>"
+
+        return html
